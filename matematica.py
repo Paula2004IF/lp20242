a5c1b6abc0320923fe5adfabf201e47805a6a6a7
Paula2004IF <lucianedepaula2004@gmail.com>
2024-09-27 13:23:47 +0000

    hkjhk

---
+++ matematica.py
@@ -0,0 +1,10 @@
+#COMANDO DEF E RETURN COM PRINT
+
+def area (largura,altura):
+    return largura*altura
+
+def volume (largura, profundidade, altura):
+    return area(largura,profundidade) * altura
+
+print(area(2,3))
+print(volume(2,3,5))   
---
+++ ola.py
@@ -0,0 +1,14 @@
+#CRIANDO O COMANDO OLÁ
+#COMANDO DEF, INPUT
+
+
+def ola (nome):
+    print(f'Olá {nome}! Seja bem vindo(a)!')
+    
+ola('PAULA')
+ola('JOSÉ')
+ola('ANA')
+
+print('Olá Mundo!')
+
+ola (input('DIGITE SEU NOME: '))
